18ea42e3ddcfce4dbc3276409c1abd5389df78e5
Jaroslav Petrusevic <jaroslav@webas.lt>
2017-08-28 10:30:15 +0300

    add LT translation

---
+++ resources/lang/lt/addon.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'title' => 'Nustatymai',
+    'name' => 'Nustatymų modulis',
+    'description' => 'Nustatymų valdymo įrankis',
+    'section' => [
+        'system' => 'Sistema',
+        'themes' => 'Temos',
+        'plugins' => 'Įskiepiai',
+        'modules' => 'Moduliai',
+        'extensions' => 'Plėtiniai',
+        'field_types' => 'Lauko tipai',
+    ],
+];
